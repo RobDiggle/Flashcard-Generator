ee066de6b78399a5ecc236ed9669bfe68dfd0828
Robert Diggle <Robschach13@yahoo.com>
2017-04-07 20:56:00 -0400

    Commit 6

--- flashCard.js
+++ flashCard.js
@@ -1,3 +1,5 @@
+
+
 function basicCard(front, back) 
 { 
 	this.front = front;
@@ -29,7 +31,7 @@ function clozeCard(text, cloze)
 	console.log("Letters in cloze: " + letters);
 	console.log("Quote with cloze removed: " + separate);
 
-	document.write("<p> Quote with cloze replaced: " + text.replace(cloze, "_________" ));
+	// document.write("<p> Quote with cloze replaced: " + text.replace(cloze, "_________" ));
 
 	}
 
@@ -52,7 +54,7 @@ function clozeCard(text, cloze)
 	// {
 	// 	if()
 	// 	{
-
+	//		console.log("Error")
 	// 	}
 	// }
 
@@ -79,6 +81,30 @@ console.log("_____________________________________________")
 
 var wittgensteinQuoteThree = new clozeCard("Philosophy aims at the logical clarification of thoughts. Philosophy is not a body of doctrine but an activity. A philosophical work consists essentially of elucidations. Philosophy does not result in 'philosophical propositions', but rather in the clarification of propositions. Without philosophy thoughts are, as it were, cloudy and indistinct: its task is to make them clear and to give them sharp boundaries. (4.112)",
  "not a body of doctrine but an activity");
+
+//There ought to be a way for the user to generate there own flashcards.
+$(document).ready(function(){
+
+$('#generator').click(function()
+{
+	console.log("Button clicked.")
+})
+	var quotes= 
+	[
+	"Logic takes care of itself; all we have to do is to look and see how it does it.",
+	"My difficulty is only an — enormous — difficulty of expression.",
+	"Don't get involved in partial problems, but always take flight to where there is a free view over the whole single great problem, even if this view is still not a clear one.",
+	"It is one of the chief skills of the philosopher not to occupy himself with questions which do not concern him.",
+	"Certainly it is correct to say: Conscience is the voice of God.",
+	"What do I know about God and the purpose of life? I know that this world exists.",
+
+	];
+
+	var clozeQuote =['Logic','expression','partial problems','questions which do not concern him.','Conscience','mystical','purpose of life']	]
+
+
+
+});
 
 
 
